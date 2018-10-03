43ce8fd1f263f78ddb34072f8f8388721e227b4d
ganto1812 <ganto1812@gmail.com>
2018-10-03 13:24:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let charisma;
+let charismaMod;
+charismaMod = (charisma - 10) / 2;
+let eventDifficulty;
+let dice20;
+let success = boolean;
+
+if (dice20 + charismaMod >= eventDifficulty) {
+    success = true;
+}
+else {
+    success = false;
+}
