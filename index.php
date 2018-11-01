82bdd7648f3a6a7c2db5b40595d0d8d31baadc5b
ganto1812 <ganto1812@gmail.com>
2018-11-01 21:00:07 +0000

    php parts

--- index.php
+++ index.php
@@ -1,52 +1,9 @@
 <?php
     session_start();
+    require "header.php";
 ?>
 
-<!DOCTYPE html>
-<html>
 
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>RPG Game</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="index.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-</head>
-
-<header>
-    <!-- Navbar -->
-    <div class="navbar fixed-top navbar-expand-lg navbar-dark" id="navbar">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active"><a class="nav-link" href="#home">Home</a></li>
-            <li class="nav-item"><a class="nav-link" href="Account/index.html">Account</a></li>
-            <li class="nav-item"><a class="nav-link" href="Settings/index.html">Settings</a></li>
-            <li class="nav-item"><a class="nav-link" href="About/index.html">About</a></li>
-            <li class="nav-item"><a class="nav-link" href="Help/index.html">Help</a></li>
-        </ul>
-        <form class="form-inline">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn my-2 my-sm-0 btn-dark" type="submit">Search</button>
-        </form>
-        <?php
-            if (isset($_SESSION['userId'])) {
-                echo '<form action="includes/logout.php" method="GET"><button type="submit" name="logoutBtn" class="btn btn-outline-warning m-3">Log Out</button></form>';
-            ;}
-    
-            if (isset($_GET['signup'])) {
-                if ($_GET['signup'] == "success") {
-            echo '<div class="alert alert-success p-4 fixed-top" role="alert">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                          </button>
-                You have signed up sucessfully!
-            </div>';}}
-        ?>
-    </div>
-</header>
 
 <main>
     <?php
@@ -59,34 +16,7 @@
             </div>';
         }
         else {
-            echo '<!-- Login Box -->
-    <div id="logInWindow" class="logIn">
-        <div class="logInWindow">
-            <form class="form" action="includes/login.php" method="POST">
-                <div class="imgcontainer">
-                    <img src="pics/hero.svg" alt="image" class="loginIcon">
-                </div>
-                <div class="miniContainer">
-                    <label for="loginUser"></label>
-                    <input type="text" placeholder="Username/E-mail" name="loginUser" required>
-                    <label for="loginPsw"></label>
-                    <input type="password" placeholder="Password" name="loginPwd" required>
-                    <button type="submit" name="loginBtn" class="login btn">Login</button>
-                    <label class="remember">
-                    <input type="checkbox" checked="checked" name="remember">
-                    Remember me
-                    </label>
-                    </div>
-
-                <div class="miniContainer">
-                    <span class="psw">Forgot <a href="#">password?</a></span>
-                    <span class="signUp d-block">Don\'t have an account?
-                        <a href="#" data-toggle="modal" data-target=".signupModal">SignUp</a>
-                    </span>
-                </div>
-            </form>
-        </div>
-    </div>';}
+            require "loginBox.php";}
     ?>
 
     <!-- Big Container -->
